dffb93f2c606e966547fd41a9223236999f36ed9
chiehan <chieh.an.hsu@gmail.com>
2016-11-26 21:38:01 +0800

    init rename sutra

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ const newTextPath = './new-pb-texts';
 const tagClass = process.argv[2];
 const keyNum = Number(process.argv[3]);
 const rangeSetting = process.argv[4];
-const sutraRegex = /(<sutra id="[0-9a-zA-Z]+?)(\d+)([^\d>]*"\/>)/g;
+const sutraNumRegex = /(<sutra id="[0-9a-zA-Z]+?)(\d+)([^\d>]*"\/>)/g;
+const sutraNameRegex = /(<sutra id=")([^>]*?)("\/>)/g;
 
 import {getTextAndNames, writeFiles} from './processFile.js';
 import {getRange} from './getRange.js';
 import {shiftMiddleNum} from './shiftMiddleNum.js';
 import {reOrderMiddleNum} from './reOrderMiddleNum.js'; 
+import (renameSutra) from './renameSutra.js';
 
 let textsAndNames = getTextAndNames(oldTextPath);
 let texts = textsAndNames.texts;
@@ -27,9 +29,11 @@ function shiftTagId(texts, rangeSetting, tagClass, keyNum) {
 
   switch (tagClass) {
     case 'shift-sutra':
-      return shiftMiddleNum(texts, sutraRegex, keyNum, start, end);
+      return shiftMiddleNum(texts, sutraNumRegex, keyNum, start, end);
     case 'reorder-sutra':
-      return reOrderMiddleNum(texts, sutraRegex, keyNum, start, end);
+      return reOrderMiddleNum(texts, sutraNumRegex, keyNum, start, end);
+    case 'rename-sutra':
+      return renameSutra(texts, sutraNameRegex);
     default:
       console.log('should match following command');
       console.log('node index.js shift-[sutra] [shift number] [grq,lsq]');
