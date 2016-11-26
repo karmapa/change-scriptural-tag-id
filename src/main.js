c7fa4c5c7c0e022ddb1db39e5c687086de91b3a7
chiehan <chieh.an.hsu@gmail.com>
2016-11-26 21:57:29 +0800

    detect 3th process arg number or sutra id

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const oldTextPath = './old-texts';
 const newTextPath = './new-pb-texts';
 const tagClass = process.argv[2];
-const keyNum = Number(process.argv[3]);
+const majorArg = process.argv[3];
 const rangeSetting = process.argv[4];
 const sutraNumRegex = /(<sutra id="[0-9a-zA-Z]+?)(\d+)([^\d>]*"\/>)/g;
 const sutraNameRegex = /(<sutra id=")([^>]*?)("\/>)/g;
@@ -16,28 +16,37 @@ let textsAndNames = getTextAndNames(oldTextPath);
 let texts = textsAndNames.texts;
 let fileNames = textsAndNames.fileNames;
 
-let newTexts = shiftTagId(texts, rangeSetting, tagClass, keyNum);
+let newTexts = shiftTagId(texts, rangeSetting, tagClass, majorArg);
 
 if (newTexts) {
   writeFiles(newTexts, fileNames, newTextPath);
 }
 
-function shiftTagId(texts, rangeSetting, tagClass, keyNum) {
+function shiftTagId(texts, rangeSetting, action, majorArg) {
   let range = getRange(rangeSetting);
   let start = range.start;
   let end = range.end;
+  let keyNum, firstSutraId;
 
-  switch (tagClass) {
+  if (! isNaN(Number(majorArg))) {
+    keyNum = Number(majorArg);
+  }
+  else {
+    firstSutraId = majorArg;
+  }
+
+  switch (action) {
     case 'shift-sutra':
       return shiftMiddleNum(texts, sutraNumRegex, keyNum, start, end);
     case 'reorder-sutra':
       return reOrderMiddleNum(texts, sutraNumRegex, keyNum, start, end);
     case 'rename-sutra':
-      return renameSutra(texts, sutraNameRegex);
+      return renameSutra(texts, sutraNameRegex, firstSutraId);
     default:
       console.log('should match following command');
       console.log('node index.js shift-[sutra] [shift number] [grq,lsq]');
       console.log('node index.js reorder-[sutra] [first number] [gre,lss]');
+      console.log('node index.js rename-[sutra] [first number]');
       break;
   }
 }
