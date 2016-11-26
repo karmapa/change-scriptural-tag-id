c15b792474e44cdf595097c7faa0d82fcbbdf979
chiehan <chieh.an.hsu@gmail.com>
2016-11-26 22:16:49 +0800

    can rename sutra

--- src/renameSutra.js
+++ src/renameSutra.js
@@ -1,3 +1,11 @@
-export function renameSutra() {
-
+export function renameSutra(texts, regex, firstSutraId) {
+  let sutraPrefix = firstSutraId.match(/.*[^\d]/)[0];
+  let sutraNum = Number(firstSutraId.match(/\d+/g).pop());
+  return texts.map((text) => {
+    return text.replace(regex, (wholeTag, preStr, sutraId, postStr) => {
+      let numStr = String(sutraNum);
+      sutraNum++;
+      return preStr + sutraPrefix + numStr + postStr;
+    });
+  });
 };
